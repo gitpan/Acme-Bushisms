649661a5782937e6eaef21023833f0eaacef8a95
Michael Accardo <accardo@cpan.org>
2003-07-21 01:51:59 +0000

    Import of ACCARDO/Acme-Bushisms-0.02 from CPAN.
    gitpan-cpan-distribution: Acme-Bushisms
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         ACCARDO/Acme-Bushisms-0.02.tar.gz
    gitpan-cpan-author:       ACCARDO
    gitpan-cpan-maturity:     released

--- Bushisms.pm
+++ Bushisms.pm
@@ -1,21 +1,29 @@
 package Acme::Bushisms;
-$VERSION = '0.01';
+$VERSION = '0.02';
 $arab = " \t"x8;
 sub invade { local $_ = unpack "b*", pop; tr/01/ \t/; s/(.{9})/$1\n/g; $arab.$_ }
 sub leave  { local $_ = pop; s/^$arab|[^ \t]//g; tr/ \t/01/; pack "b*", $_ }
 sub oil    { $_[0] =~ /\S/ }
 sub drill  { $_[0] =~ /^$arab/ }
 
-open 0 or print "Can't leave '$0'\n" and exit;
-my $iraq = join "", <0>;
+open 0 or print "Can't open '$0'\n" and exit;
+$iraq = join "", <0>;
 $iraq =~ s/.*^\s*use\s+Acme::Bushisms\s*;\n\n(?:.*?George.*?\n)?//sm;
 local $SIG{__WARN__} = \&oil;
 do {eval leave $iraq; exit} unless oil $iraq && not drill $iraq;
 
-use LWP::Simple;
-$list = get("http://slate.msn.com/id/76886/");
-($bush) = $list =~ /(<\/p><p>.*?\".*<\/p><p>)/sm;
-$bush =~ s/\(thank.*?\)//ismg;
+use LWP::Simple qw($ua get);
+$ua ->timeout(10);
+$lies = get("http://slate.msn.com/id/76886/");
+if (not defined $lies) {
+$dubya =<<EOF; 
+"I know the human being and fish can coexist peacefully."
+--George W Bush, Saginaw, Mich., Sept. 29, 2000
+EOF
+}
+else {
+($bush) = $lies =~ /(<\/p><p>.*?\".*<\/p><p>)/sm;
+$bush =~ s/\(\s*thank.*?\)//ismg;
 $bush =~ s/<\/?[^p][^>]+>//g;
 $bush =~ s/\&\w+\;//g;
 $bush =~ s/("--|("|'|\s)[^\w\s\r\"\.\']{3,3})/"\n--George W Bush, /ig;
@@ -24,7 +32,9 @@ $bush =~ s/[^\w\s\r\"\.\']{3,3}/--/g;
 $dubya = $quotes[rand @quotes];
 $dubya = $dubya . "\n--George W Bush"
   unless ($dubya =~ /--George/);
+}
 $dubya =~ s/\s+$//;
+
 
 open 0, ">$0" or print "Cannot invade '$0'\n" and exit;
 print {0} "use Acme::Bushisms;\n\n$dubya\n", invade $iraq and exit;
@@ -47,24 +57,16 @@ removes all the unsightly printable, democrat, and liberal characters from
 your source file. The code continues to work exactly as it did before, 
 but now it contains Bush speak:
 
-       use Acme::Bushisms;
+      	use Acme::Bushisms;
 
-       "And so, in my State of the -- my State of the Union -- or 
-	 state -- my speech to the nation, whatever you want to call it, 
-       speech to the nation -- I asked Americans to give 4,000 years -- 
-       4,000 hours over the next -- the rest of your life -- of service 
-	 to America. That's what I asked -- 4,000 hours." 
-       --George W Bush, Bridgeport, Conn., April 9, 2002
+	"Families is where our nation finds hope, where wings take dream."
+	--George W Bush, LaCrosse, Wis., Oct. 18, 2000
         
 =head1 NOTES
 
 The Bushisms are random quotes from the Complete Bushisms Website by 
 Jacob Weisberg on Slate. The address is http://slate.msn.com/id/76886
-If this address changes, then this module will fail to work. I will 
-fix that problem in a future release.  
-
-Special thanks to Perl Monks for helping me understand L<Acme::Bleach>
-And, of course, to Damian Conway.
+If this page is unreachable, then a default Bushism is used. 
 
 =head1 AUTHOR
 
